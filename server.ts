7f540bf8ffc8f9778d838c7294b8f6babb091d1b
joey desjardins <jwdesjardin@gmail.com>
2021-03-13 13:26:23 -0800

    allow cors

--- server.ts
+++ server.ts
@@ -1,29 +1,31 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 console.log('server', process.env.SERVER_API_KEY)
 
-const app = express();
+const app = express()
 
-express.json();
+express.json()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
-	try {
-		res.send('Welcome to the NHL Stats API');
-	} catch (error) {
-		console.log('error getting team standings');
-	}
-});
+  try {
+    res.send('Welcome to the NHL Stats API')
+  } catch (error) {
+    console.log('error getting team standings')
+  }
+})
 
 import nhlRoutes from './routes'
 
 app.use('/api', nhlRoutes)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-	console.log(`Server is listening on port ${PORT}`);
-});
-
+  console.log(`Server is listening on port ${PORT}`)
+})
