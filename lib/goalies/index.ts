f2c62a852498143062a761bad8282d035c06da56
joey desjardins <jwdesjardin@gmail.com>
2021-03-15 12:21:46 -0700

    remove duplicate rows for goalies that have been traded

--- lib/goalies/index.ts
+++ lib/goalies/index.ts
@@ -19,25 +19,31 @@ export const getGoalieScoring = async () => {
     $('tr', table).each((_idx, row) => {
       if ($('td[data-stat="player"]', row).text() === '') return
 
-      let goalie: Goalie = {
-        player_id: $('td[data-stat="player"]', row).attr('data-append-csv') || '',
-        player: $('td[data-stat="player"]>a', row).text(),
-        age: parseInt($('td[data-stat="age"]', row).text()),
-        team_id: $('td[data-stat="team_id"]>a', row).text(),
-        position: 'G',
-        games_played: parseInt($('td[data-stat="games_goalie"]', row).text()),
-        games_started: parseInt($('td[data-stat="starts_goalie"]', row).text()),
-        wins: parseInt($('td[data-stat="wins_goalie"]', row).text()),
-        losses: parseInt($('td[data-stat="losses_goalie"]', row).text()),
-        ot_losses: parseInt($('td[data-stat="ties_goalie"]', row).text()),
-        goals_against: parseInt($('td[data-stat="goals_against"]', row).text()),
-        shots_against: parseInt($('td[data-stat="shots_against"]', row).text()),
-        saves: parseInt($('td[data-stat="saves"]', row).text()),
-        save_percentage: parseFloat($('td[data-stat="save_pct"]', row).text()),
-        goals_against_average: parseFloat($('td[data-stat="goals_against_avg"]', row).text()),
-        shutouts: parseInt($('td[data-stat="shutouts"]', row).text()),
+      const player_id = $('td[data-stat="player"]', row).attr('data-append-csv')
+
+      const duplicate = goalieScoring.find((goalie) => goalie.player_id === player_id)
+
+      if (player_id && !duplicate) {
+        let goalie: Goalie = {
+          player_id: $('td[data-stat="player"]', row).attr('data-append-csv') || '',
+          player: $('td[data-stat="player"]>a', row).text(),
+          age: parseInt($('td[data-stat="age"]', row).text()),
+          team_id: $('td[data-stat="team_id"]>a', row).text(),
+          position: 'G',
+          games_played: parseInt($('td[data-stat="games_goalie"]', row).text()),
+          games_started: parseInt($('td[data-stat="starts_goalie"]', row).text()),
+          wins: parseInt($('td[data-stat="wins_goalie"]', row).text()),
+          losses: parseInt($('td[data-stat="losses_goalie"]', row).text()),
+          ot_losses: parseInt($('td[data-stat="ties_goalie"]', row).text()),
+          goals_against: parseInt($('td[data-stat="goals_against"]', row).text()),
+          shots_against: parseInt($('td[data-stat="shots_against"]', row).text()),
+          saves: parseInt($('td[data-stat="saves"]', row).text()),
+          save_percentage: parseFloat($('td[data-stat="save_pct"]', row).text()),
+          goals_against_average: parseFloat($('td[data-stat="goals_against_avg"]', row).text()),
+          shutouts: parseInt($('td[data-stat="shutouts"]', row).text()),
+        }
+        goalieScoring.push(goalie)
       }
-      goalieScoring.push(goalie)
     })
 
     return goalieScoring
